3145c21c19477434188975dc4ce9dcd94d732f97
xeron07 <ni.xeron07@gmail.com>
2026-06-05 15:31:07 +0600

    fix : node version on deploy and some file org

--- vite.config.ts
+++ vite.config.ts
@@ -37,6 +37,7 @@ export default defineConfig({
         ]
       },
       workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5MB
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
         runtimeCaching: [
           {
